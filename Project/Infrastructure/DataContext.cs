8d41dc4e72876043f954981b232f79529a1536a4
JobranZaiter <zjoubran7@gmail.com>
2023-09-11 08:52:02 -0700

    Initial push

---
+++ Project/Infrastructure/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project.Infrastructure
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    { 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+              public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        
+    }
+}
---
+++ Project/Infrastructure/NewFolder/SmallCartViewComponent .cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using Project.Models.ViewModel;
+
+namespace Project.Infrastructure.NewFolder
+{
+    public class SmallCartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+            SmallCartViewModel smallCartVM;
+
+            if(cart == null || cart.Count == 0)
+            {
+                smallCartVM = null;
+            }
+
+            else
+            {
+                smallCartVM = new()
+                {
+                    NumberOfItems = cart.Sum(x => x.Quantity),
+                    TotalAmount = cart.Sum(x => x.Quantity * x.Price)
+                };
+            }
+
+            return View(smallCartVM);
+
+        }
+
+
+    }
+        
+}
